b399f10a79b1ba599f78fe340a247533958d0266
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2021-11-29 22:09:10 +0200

    added test for adding multiple different items and price being correct

--- week4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ week4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,7 +6,22 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.cart = Ostoskori()
+        self.item = Tuote("test", 5)
+        self.milk = Tuote("milk", 1)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.cart.hinta(), 0)
 
+    def test_add_single_item(self):
+        self.cart.lisaa_tuote(self.item)
+        self.assertEqual(len(self.cart), 1)
+
+    def test_add_different_items(self):
+        self.cart.lisaa_tuote(self.item)
+        self.cart.lisaa_tuote(self.milk)
+        self.assertEqual(len(self.cart), 2)
+
+    def test_add_different_items_price_correct(self):
+        self.cart.lisaa_tuote(self.item)
+        self.cart.lisaa_tuote(self.milk)
+        self.assertEqual(self.cart.hinta(), 6)
