4b3aee31bb6de2ea97ea4a951328b8588ee7c796
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2021-12-04 22:11:01 +0200

    made get_score equal case also use dictionary instead of ifs

--- week5/tennis/src/tennis_game.py
+++ week5/tennis/src/tennis_game.py
@@ -10,20 +10,17 @@ class TennisGame:
             self.home_score = self.home_score + 1
         else:
             self.away_score = self.away_score + 1
+    def __check_equal(self):
+        pass
 
     def get_score(self):
         score = ""
         temp_score = 0
+        points_to_str = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
 
         if self.home_score == self.away_score:
-            if self.home_score == 0:
-                score = "Love-All"
-            elif self.home_score == 1:
-                score = "Fifteen-All"
-            elif self.home_score == 2:
-                score = "Thirty-All"
-            elif self.home_score == 3:
-                score = "Forty-All"
+            if self.home_score in points_to_str:
+                score = "{}-All".format(points_to_str[self.home_score])
             else:
                 score = "Deuce"
 
@@ -39,6 +36,5 @@ class TennisGame:
             else:
                 score = "Win for player2"
         else:
-            points_to_str = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
             score = "{}-{}".format(points_to_str[self.home_score], points_to_str[self.away_score])
         return score
